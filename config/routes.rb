749a9b3e824e238ed6117e6efbb070437bcc8893
tellone <filip.diloom@gmail.com>
2013-02-24 13:44:27 +0100

    added the user contoller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ DocumentStorage::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+
+  root :to => "user#index"
+  
+  
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
