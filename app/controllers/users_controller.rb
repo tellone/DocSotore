08af60c60578ad612c58b1243ebffd812d9709a4
tellone <filip.diloom@gmail.com>
2013-02-24 17:54:15 +0100

    create implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+    
+    redirect_to :action => "index"  
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe UsersController do
+ describe "User Create" do
+   before :each do 
+     @user = mock_model(User)
+   end
+ 
+   it "crates a new user" do
+     # pending("a mock model nice")
+     User.should_receive(:new).with.("string" => "CaptianObvious@obv.se").and_return(@user)
+      post :create, user => { "email" => "CaptianObvious@obv.se"}
+  end
+   
+   it "saves the user" do
+      User.stub(:new).and_return(@user)
+      @user.should_receive(:save)
+      post :create
+   end
+   it "rediracts to index" do
+     post :create
+     response.should redirect_to( :action => "index")
+   end
+ end
 
 end
