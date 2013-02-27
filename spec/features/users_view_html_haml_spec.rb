18b3a539ed0717afe8b3d6b35671946121c12e43
tellone <filip.diloom@gmail.com>
2013-02-27 13:36:43 +0100

    refreatoring user view

--- spec/features/users_view_html_haml_spec.rb
+++ spec/features/users_view_html_haml_spec.rb
@@ -5,7 +5,6 @@ describe "the user view" do
       visit '/users'
     end
     it "Has a header" do
-      visit '/users'
       page.should have_content("Wellcome to the Document Storage!")
     end
     it "has a link to sign up page" do
@@ -13,7 +12,6 @@ describe "the user view" do
       page.should have_link('recover lost password')
     end
     it " has a login form" do
-      visit '/users'
       within('#login_form') do
         fill_in 'Email', :with => 'gotoexaple@obv.se'
         fill_in 'Password', :with =>'secuity!'
@@ -35,15 +33,38 @@ describe "the user view" do
     before :each do
       
       @user1 = FactoryGirl.create(:user)
-      @user2 = FactoryGirl.create(:user)
+      # @user2 = FactoryGirl.create(:user)
+      visit '/users'
     end
 
     it "displayes profile and links to edit" do
 
-      visit user_path(@user1)
+      click_link(@user1.email)
       page.should have_content(@user1.email)
       page.should have_link("edit profile")
+      page.should have_link("delete user")
     end  
+    
+    it "Has options for own documents" do
+      
+      doc1 = FactoryGirl.create(:document)
+      visit user_path(@user1)
+      page.should have_content("Documents")
+      pending("set up the documet model")
+      page.should have_link("doc1.title")
+    end
+  end
+  describe "users/edit.html.haml" do
+  before :each do
+      
+      @user1 = FactoryGirl.create(:user)
+      # @user2 = FactoryGirl.create(:user)
+      visit edit_user_path(@user1)
+    end
+ 
+    it "Displays an edit form with email and password" do
+      pending
+    end
   end
 end
     
